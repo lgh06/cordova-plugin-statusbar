bf227d695d1f29e479dc748f4820ea711d39cabd
Daniel Liu <hnnk@qq.com>
2023-03-23 11:16:22 +0800

    Update StatusBar.java

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -122,11 +122,12 @@ public class StatusBar extends CordovaPlugin {
 
             case ACTION_HIDE:
                 activity.runOnUiThread(() -> {
-                    int uiOptions = window.getDecorView().getSystemUiVisibility()
+                    int uiOptions = 
+                        // window.getDecorView().getSystemUiVisibility()
                         // | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         // | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         // | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
                         // | View.SYSTEM_UI_FLAG_FULLSCREEN;
 
